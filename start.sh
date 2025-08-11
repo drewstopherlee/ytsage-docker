2601ca57ff8780f674ed3befa0854b1a680e6fff
Andrew Shaffer <57060193+drewstopherlee@users.noreply.github.com>
2025-08-11 16:56:15 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Start a virtual X server
-Xvfb :99 -screen 0 1920x1080x24 &
+Xvfb :99 -screen 0 1280x720x24 &
 
 # Start a lightweight window manager
 fluxbox &
 
 # Start x11vnc server
-x11vnc -display :99 -nopw -forever &
+x11vnc -display :99 -forever -nopw -quiet -ncache 10 &
 
 # Start noVNC
 websockify --web /opt/novnc 8080 localhost:5900 &
